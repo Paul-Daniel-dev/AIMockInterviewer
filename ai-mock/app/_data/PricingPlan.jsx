2f808332e89e6d8cddef14bb412e8fa577734321
Paul-Daniel Pretorius <165285046+PaulDPretorius@users.noreply.github.com>
2024-06-16 20:26:10 +0200

    Header setup

---
+++ ai-mock/app/_data/PricingPlan.jsx
@@ -0,0 +1,14 @@
+export default[
+    {
+        link:'https://buy.stripe.com/test_8wMaEZeHVcfk1uU4gj',
+        price:7.99,
+        priceId:'price_1PS2hJP6hQWgvByIMN05Nt4W',
+        duration:'Monthly'
+    },
+    {
+        link:'https://buy.stripe.com/test_8wM00l7ft5QW7Ti000',
+        price:49.99,
+        priceId:'price_1PS2dMP6hQWgvByI5djea9FA',
+        duration:'Yearly'
+    }
+]
---
+++ ai-mock/app/dashboard/_components/Header.jsx
@@ -0,0 +1,43 @@
+"use client"
+import { UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+function Header() {
+  
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path)
+  },[])  
+    
+  return (
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-md' >
+        <Image src={"/logo.svg"} width={80} height={80} alt='logo'/>
+        <ul className='hidden md:flex gap-6'>
+        <Link href={'/dashboard'}>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path == '/dashboard' && 'text-primary font-bold'}
+            `}>
+                
+                Dashboard</li>
+                </Link>
+            
+            <Link href={'/dashboard/upgrade'}>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path == '/dashboard/upgrade' && 'text-primary font-bold'}
+            `}>Upgrade</li>
+            </Link>
+            <Link href={'/dashboard/how'}>
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+            ${path == '/dashboard/how' && 'text-primary font-bold'}
+            `}>How does it work?</li>
+            </Link>
+        </ul>
+        <UserButton/>
+    </div>
+  )
+}
+
+export default Header
