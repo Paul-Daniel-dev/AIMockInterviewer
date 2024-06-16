2e409c9859efb0f9d266a6460f6be61f81f3143c
Paul-Daniel Pretorius <165285046+PaulDPretorius@users.noreply.github.com>
2024-06-16 20:14:38 +0200

    Further Clerk Setup

---
+++ ai-mock/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,26 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main>
+      <div className="flex flex-col justify-center items-center pt-40">
+      <div>
+          <SignIn />
+        </div>
+        <div className="mt-4">
+          <h1 className="text-black p-2 font-bold text-xl">
+            For testing purposes a dummy account has already been created for
+            you
+          </h1>
+          <h1 className="text-black p-2 font-bold text-xl">
+            Email: aimockinterviewer@gmail.com
+          </h1>
+          <h1 className="text-black p-2 font-bold text-xl">
+            Password: zxcvbnm12345@
+          </h1>
+        </div>
+        
+      </div>
+    </main>
+  );
+}
